50795d7d2a398c3a03bfc15aacf0f62c14d3aa6d
Yara Daudt <yaracrisalis@gmail.com>
2024-12-19 12:35:47 +0000

    :sparkles: feat: Summary

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,9 +1,11 @@
 import { Header } from '../../components/Header';
+import { Summmary } from '../../components/Summary';
 
 export function Transactions() {
   return (
     <div>
       <Header />
+      <Summmary />
     </div>
   );
 }
