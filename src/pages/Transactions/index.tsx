5339128d4c89a468a30f340d06d8e1e38fe62629
Yara Daudt <yaracrisalis@gmail.com>
2024-12-19 14:19:04 +0000

    :sparkles: feat: Transactions page

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,40 @@
-import { Header } from '../../components/Header';
-import { Summmary } from '../../components/Summary';
+import { Header } from "../../components/Header";
+import { Summmary } from "../../components/Summary";
+import { SearchForm } from "./components/SearchForm";
+import {
+  PriceHighlight,
+  TransactionsContainer,
+  TransactionsTable,
+} from "./styles";
 
 export function Transactions() {
   return (
     <div>
       <Header />
       <Summmary />
+      <TransactionsContainer>
+        <SearchForm />
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>23/12/2024</td>
+            </tr>
+            <tr>
+              <td width="50%">Hambúrguer</td>
+              <td>
+                <PriceHighlight variant="outcome">- R$ 59,00</PriceHighlight>
+              </td>
+              <td>Alimentação</td>
+              <td>18/12/2024</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   );
 }
