db9859716e476ebc8cacc3fa7b125c81e3211b3a
Yara Daudt <yaracrisalis@gmail.com>
2025-01-03 18:02:52 +0000

    :sparkles: feat: Transactions Query

--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -1,41 +1,53 @@
 import { createContext, ReactNode, useState, useEffect } from "react";
 
 interface Transaction {
-    id: number;
-    description: string;
-    type: "income" | "outcome";
-    price: number;
-    category: string;
-    createdAt: string;
-  }
+  id: number;
+  description: string;
+  type: "income" | "outcome";
+  price: number;
+  category: string;
+  createdAt: string;
+}
 
 interface TransactionsContextType {
-    transactions: Transaction[];
+  transactions: Transaction[];
+  fetchTransactions: (query?: string) => Promise<void>;
 }
 
 interface TransactionProviderProps {
-    children: ReactNode;
+  children: ReactNode;
 }
 
 export const TransactionsContext = createContext({} as TransactionsContextType);
 
-export function TransactionsProvider({children}: TransactionProviderProps) {
-    const [transactions, setTransactions] = useState<Transaction[]>([]);
+export function TransactionsProvider({ children }: TransactionProviderProps) {
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
 
-    async function loadTransactions() {
-      const response = await fetch("http://localhost:3000/transactions");
-      const data = await response.json();
-  
-      setTransactions(data);
+  async function fetchTransactions(query?: string) {
+    const url = new URL("http://localhost:3000/transactions");
+
+    if (query) {
+      url.searchParams.append("q", query);
     }
-  
-    useEffect(() => {
-      loadTransactions();
-    }, []);
-  
-    return (
-        <TransactionsContext.Provider value={{ transactions }}>
-            {children}
-        </TransactionsContext.Provider>
-    )
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    setTransactions(data);
+  }
+
+  useEffect(() => {
+    fetchTransactions();
+  }, []);
+
+  return (
+    <TransactionsContext.Provider
+      value={{
+        transactions,
+        fetchTransactions,
+      }}
+    >
+      {children}
+    </TransactionsContext.Provider>
+  );
 }
--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -1,8 +1,10 @@
+import { useContext } from "react";
 import { useForm } from "react-hook-form";
-import { MagnifyingGlass } from "phosphor-react";
-import { SearchFormContainer } from "./styles";
+import { TransactionsContext } from "../../../../contexts/TransactionsContext";
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { MagnifyingGlass } from "phosphor-react";
+import { SearchFormContainer } from "./styles";
 
 const searchFormSchema = z.object({
   query: z.string(),
@@ -11,6 +13,7 @@ const searchFormSchema = z.object({
 type SearchFormInputs = z.infer<typeof searchFormSchema>;
 
 export function SearchForm() {
+  const { fetchTransactions } = useContext(TransactionsContext)
   const {
     register,
     handleSubmit,
@@ -20,9 +23,7 @@ export function SearchForm() {
   });
 
   async function handleSearchTransactions(data: SearchFormInputs) {
-    await new Promise((resolve) => setTimeout(resolve, 2000));
-
-    console.log(data);
+    await fetchTransactions(data.query)
   }
 
   return (
